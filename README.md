ed54c5381b41cce3da633f150bea21addf4a64d9
Sebastian Huebner <sebastian@hueb-ner.de>
2018-09-17 13:55:39 +0200

    Inital commit

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # emoji-downloader
 
-Download custom emoji's from Mastodon or Pleroma instances.
+Do you ever thought: "Oh, this instance have nice emoji's, I want them."? Then just use this script to download them.
+
+## Dependencies
+
+* lua (>=5.1)
+* [lua-requests](https://github.com/JakobGreen/lua-requests)
+
+## Install
+
+Since this script depends on [lua-requests](https://github.com/JakobGreen/lua-requests), you need [luarocks](https://luarocks.org/#quick-start) to install it.
+If you have **luarocks** installed, install **lua-requests**:
+
+```
+luarocks install lua-requests
+```
+
+After this is done, clone this repository and run the script like this:
+
+```
+lua emoji-downloader.lua "https://example.com"
+```
+
+
+
+## Works with
+
+* [Pleroma](https://pleroma.social)
+* [Mastodon](https://joinmastodon.org)
---
+++ emoji-downloader.lua
@@ -0,0 +1,84 @@
+#!/usr/bin/env lua
+--
+-- Download emojis from mastodon or pleroma instances
+--
+-- Copyright © 2018 Sebastian Huebner
+-- This work is free. You can redistribute it and/or modify it under the
+-- terms of the Do What The Fuck You Want To Public License, Version 2,
+-- as published by Sam Hocevar. See the LICENSE file or http://www.wtfpl.net/ 
+-- for more details.
+
+local requests = require('requests')
+local lfs = require('lfs')
+
+local download_folder = './download/'
+local custom_emoji_path = '/api/v1/custom_emojis'
+local url
+
+-- checks if file exists
+local function exists(file)
+  local ok, err, code = os.rename(file, file)
+
+  if not ok then
+    if code == 13 then
+      return true
+    end
+  end
+
+  return ok, err
+end
+
+-- returns the url if valid, if not it return nil
+local function validate_url(url)
+  return url:match('^https?://[%w%.]+%w+/?$')
+end
+
+-- shows help message
+local function show_help()
+  io.write('Use the script like this:\n' ..
+    '  lua emoji_downloader.lua "https://example.com"\n\n')
+  os.exit(1)
+end
+
+-- downloads and saves the emoji
+local function download_file(url, shortcode, folder)
+  local file_name = shortcode .. url:match('https?://.+/.*(%.%w+)[%?%d]*$')
+  local resp, err = requests.get(url)
+
+  if not resp then return nil, err end
+  
+  local file = io.open(folder ..'/' .. file_name, 'w')
+  file:write(resp.text)
+  file:close()
+  
+  return true  
+end
+
+-- main function
+local function main(url, ce_path, d_folder)
+  if not exists(d_folder) then lfs.mkdir(d_folder) end
+
+  local emoji_list = assert(requests.get(url .. ce_path).json())
+  
+  for _, e in ipairs(emoji_list) do
+    local success, err = download_file(e.url, e.shortcode, d_folder)
+    
+    if not success then
+      io.write(('The following error occured during the download of %s: %s\n'):format(e.shortcode, err))
+    end
+  end
+  
+  io.write(('Look into folder %s to find the downloaded emoji\'s!\n'):format(download_folder))
+end
+
+
+-- if argument is given and it's a valid url, remove the trailing / if given
+-- use the url without scheme as download folder
+if arg[1] and validate_url(arg[1]) then
+  url = ( arg[1]:sub(-1) == '/' and arg[1]:sub(1, -2) ) or arg[1]
+  download_folder = download_folder .. url:match('^https?://([%w%.]+%w+)/?$') .. '/'
+else
+  show_help()
+end
+
+main(url, custom_emoji_path, download_folder)
